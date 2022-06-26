80edbebbac70a67cc5b22d95773b838ab8c249a3
Metroish472 <107201519+Metroish472@users.noreply.github.com>
2022-06-25 23:55:40 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 var mod = {
     primaryName: "Ordinal",
     secondaryName: "-Layers",
-    version: "β.0",
+    version: "β.1",
     engineVer: "0.2.3 P1", //DO NOT MODIFY
     debugMode: true,
     themes: [
