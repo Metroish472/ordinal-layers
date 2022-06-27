494adce6ef6068e18081ba7f639a8a49924a18b3
Metroish472 <107201519+Metroish472@users.noreply.github.com>
2022-06-26 20:44:36 -0400

    Update endgame-tab.js

--- js/components/endgame-tab.js
+++ js/components/endgame-tab.js
@@ -11,7 +11,7 @@ Vue.component("endgame-tab", {
     },
     template: `<div id="endgame">
     <h2><span class="omega">You</span> Win!</h2>
-    <p>You beat the game in <span class="omega">{{timeSpent}}</span><br>
+    <p>You reached the ultimate goal of Infinity-Insta-Bility in <span class="omega">{{timeSpent}}</span><br>
     <button onclick="game.settings.tab = 'Layers'">Continue</button> <button @click="hardResetGame()">Wipe Game</button></p>
 </div>`
 })
