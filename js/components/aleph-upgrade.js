9040bffdfd7c348e50ee90ee6a3e1b5e19334bf6
Metroish472 <107201519+Metroish472@users.noreply.github.com>
2022-07-17 11:46:48 -0400

    Update aleph-upgrade.js

--- js/components/aleph-upgrade.js
+++ js/components/aleph-upgrade.js
@@ -1,4 +1,4 @@
 Vue.component("aleph-upgrade", {
     props: ["upgrade"],
-    template: `<resource-upgrade :upgrade="upgrade" :resourcename="'<span class=` + 'aleph' + `>↑</span>'"></resource-upgrade>`
+    template: `<resource-upgrade :upgrade="upgrade" :resourcename="'<span class=` + 'aleph' + `>?</span>'"></resource-upgrade>`
 });
