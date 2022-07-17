b0a9849153cd115b7052b939f385f8f96ae9de3d
Metroish472 <107201519+Metroish472@users.noreply.github.com>
2022-07-17 11:17:21 -0400

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,8 +1,33 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>vα</template>
+    <template v-slot:text>First pre-release
+    </template>
+    </guide-item>
+    <guide-item>
+    <template v-slot:title>vβ</template>
+    <template v-slot:text>Finished all 200 symbols for ordinal levels!
+    </template>
+    </guide-item>
+    <guide-item>
+    <template v-slot:title>vβ.1</template>
+    <template v-slot:text>Changed most of the remaining text needed
+    </template>
+    </guide-item>
+    <guide-item>
     <template v-slot:title>v1.0.0</template>
-    <template v-slot:text>When modding, edit this when you make new releases!
+    <template v-slot:text>The first release is published!
+    </template>
+    </guide-item>
+    <guide-item>
+    <template v-slot:title>v1.0.0.1</template>
+    <template v-slot:text>Fixed the screen not loading the engine
+    </template>
+    </guide-item>
+    <guide-item>
+    <template v-slot:title>v1.0.1.0</template>
+    <template v-slot:text>Added more achievements to the engine
     </template>
     </guide-item>
 </div>`
